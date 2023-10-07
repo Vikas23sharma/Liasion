1fc52ac2bffd7f92575d483712c32cf6ced472ee
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-10-07 21:12:45 +0530

    homepage created with footer

--- frontend/liasion/src/components/Allroutes.jsx
+++ frontend/liasion/src/components/Allroutes.jsx
@@ -4,11 +4,13 @@ import  SignupCard  from '../pages/Register'
 import SplitScreen from '../pages/Login'
 import Dashboard from '../pages/Dashboard'
 import Privateroute from './Privateroute'
+import CallToActionWithIllustration from '../pages/Home'
 
 
 const Allroutes = () => {
     return (
         <Routes>
+            <Route path='/' element={<CallToActionWithIllustration></CallToActionWithIllustration>}></Route>
             <Route path='/register' element={<SignupCard></SignupCard>}></Route>
             <Route path='/login' element={<SplitScreen/>}></Route>
             <Route path='/dashboard' element={<Privateroute><Dashboard/></Privateroute>}></Route>
